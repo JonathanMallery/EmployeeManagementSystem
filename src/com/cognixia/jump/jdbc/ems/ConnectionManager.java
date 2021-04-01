4f04da84b76a99f4edad58951b05e214542eb4af
JonathanMallery <malleryjm@yahoo.com>
2021-04-01 12:52:54 -0500

    connection process updated

--- src/com/cognixia/jump/jdbc/ems/ConnectionManager.java
+++ src/com/cognixia/jump/jdbc/ems/ConnectionManager.java
@@ -12,24 +12,30 @@ public class ConnectionManager {
 	
 	private static Connection connection = null;
 	
+	private static final String URL = "jdbc:mysql://localhost:3306/ems";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
+	
 	private static void makeConnection() {
 		
-		Properties props = new Properties();
+		// This is one way of making a connection by using a properties file
+//		Properties props = new Properties();
+//		
+//		try {
+//			props.load(new FileInputStream("./resources/config.properties"));
+//		} catch (FileNotFoundException e1) {
+//			e1.printStackTrace();
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		}
+//		
+//		String url = props.getProperty("url");
+//		String username = props.getProperty("username");
+//		String password = props.getProperty("password");
 		
 		try {
-			props.load(new FileInputStream("./resources/config.properties"));
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		String url = props.getProperty("url");
-		String username = props.getProperty("username");
-		String password = props.getProperty("password");
-		
-		try {
-			connection = DriverManager.getConnection(url, username, password);
+//			connection = DriverManager.getConnection(url, username, password);
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
